76b0e06ef2287fc533f57369a2a1791ee70306b8
troyBORG <troyborg@gmail.com>
2025-08-27 04:09:26 -0500

    Update BoundedUIX.cs
    Version bump

--- BoundedUIX/BoundedUIX.cs
+++ BoundedUIX/BoundedUIX.cs
@@ -56,7 +56,7 @@ namespace BoundedUIX
         public override string Author => "Banane9 & djsime1";
         public override string Link => "https://github.com/djsime1/BoundedUIX";
         public override string Name => "BoundedUIX";
-        public override string Version => "1.1.2";
+        public override string Version => "1.1.3";
 
         public override void OnEngineInit()
         {
